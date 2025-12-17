66ccea11e2028927fe13f6be4c158147816f11d1
unknown <Thomas Cagnion>
2025-12-17 23:11:51 +0100

    mise a jour site_8

--- app.js
+++ app.js
@@ -31,5 +31,37 @@ function toggleExperience() {
         } else {
             list.classList.add("hidden");
             icon.textContent = "+";
+            
+            
         }
 }
+
+function toggleDarkMode() {
+        const body = document.body;
+        const button = document.getElementById("dark-mode-toggle");
+
+        if (body.classList.contains("dark-mode")) {
+            // Passer en mode clair
+            body.classList.remove("dark-mode");
+            button.innerHTML = '<i class="fas fa-moon"></i> Mode sombre';
+            localStorage.setItem("darkMode", "false");
+        } else {
+            // Passer en mode sombre
+            body.classList.add("dark-mode");
+            button.innerHTML = '<i class="fas fa-sun"></i> Mode clair';
+            localStorage.setItem("darkMode", "true");
+        }
+    }
+    document.addEventListener("DOMContentLoaded", function() {
+        const body = document.body;
+        const button = document.getElementById("dark-mode-toggle");
+        const darkMode = localStorage.getItem("darkMode");
+
+        if (darkMode === "true") {
+            body.classList.add("dark-mode");
+            button.innerHTML = '<i class="fas fa-sun"></i> Mode clair';
+        }
+
+        // Ajouter un écouteur d'événement au bouton
+        button.addEventListener("click", toggleDarkMode);
+    });
